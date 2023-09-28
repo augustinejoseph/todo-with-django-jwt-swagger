bcf334f075b80eba09814c6531132a128228be53
augustinejoseph <augustine.augustine4@gmail.com>
2023-09-28 12:44:11 +0530

    docker file added

--- todo_list_app/models.py
+++ todo_list_app/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
-from .utils import send_reminder_email
 
 
 class TodoTask(models.Model):
